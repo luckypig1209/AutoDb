e0031e8a874ea3f28980174fe34459ff8388c25a
Administrator <zhangyug@digiwin.com>
2024-03-26 14:18:43 +0800

    Update auto_sql.sh

--- jenkins/auto_sql.sh
+++ jenkins/auto_sql.sh
@@ -3,8 +3,8 @@ if docker inspect ${APP_NAME} >/dev/null 2>&1; then
     docker rm ${APP_NAME}
 fi
 
-echo "' ${APP_NAME} ${MODULE} ${TARGET} ${tenantSid} ${tenantId}'
-echo "${APP_NAME} ${MODULE} ${TARGET} ${tenantSid} ${tenantId}"
+
+echo "${APP_NAME},${MODULE},${TARGET},${tenantSid},${tenantId}"
 
 
 docker run -itd --name ${APP_NAME} -e MODULE=${MODULE} -e TARGET=${TARGET} -e tenantSid=${tenantSid} -e tenantId=${tenantId} ${DOCKER_IMAGE_NAME}
