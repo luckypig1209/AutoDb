7f20be3fef232c6352e38600c9951ea1293e3c9d
Administrator <zhangyug@digiwin.com>
2024-03-26 14:59:59 +0800

    Update auto_sql.sh

--- jenkins/auto_sql.sh
+++ jenkins/auto_sql.sh
@@ -3,6 +3,7 @@ DOCKER_REGISTRY_URL="registry.digiwincloud.com.cn"
 DOCKER_REGISTRY_PROJECT="tbb"
 DOCKER_IMAGE_NAME="${DOCKER_REGISTRY_URL}/tbb/auto_sql:0.0.0.$1"
 
+echo "${DOCKER_IMAGE_NAME}"
 echo "hahaha"
 echo "$1
 "
