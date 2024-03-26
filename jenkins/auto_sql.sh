642b63ce453b10c70999c0cf0ad3bc5e7805fa8c
Administrator <zhangyug@digiwin.com>
2024-03-26 15:07:59 +0800

    Update auto_sql.sh

--- jenkins/auto_sql.sh
+++ jenkins/auto_sql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-APP_NAME = "auto_sql"
+
 DOCKER_REGISTRY_URL="registry.digiwincloud.com.cn"
 DOCKER_REGISTRY_PROJECT="tbb"
 DOCKER_IMAGE_NAME="${DOCKER_REGISTRY_URL}/tbb/auto_sql:0.0.0.$1"
@@ -8,13 +8,13 @@ echo "${DOCKER_IMAGE_NAME}"
 echo "hahaha"
 echo "$1"
 
-if docker inspect ${APP_NAME} >/dev/null 2>&1; then
-    docker stop ${APP_NAME}
-    docker rm ${APP_NAME}
+if docker inspect auto_sql >/dev/null 2>&1; then
+    docker stop auto_sql
+    docker rm auto_sql
 fi
 
 
-echo "${APP_NAME},${MODULE},${TARGET},${tenantSid},${tenantId}"
+echo "${MODULE},${TARGET},${tenantSid},${tenantId}"
 
 
-docker run -itd --name ${APP_NAME} -e MODULE=${MODULE} -e TARGET=${TARGET} -e tenantSid=${tenantSid} -e tenantId=${tenantId} ${DOCKER_IMAGE_NAME}
+docker run -itd --name auto_sql -e MODULE=${MODULE} -e TARGET=${TARGET} -e tenantSid=${tenantSid} -e tenantId=${tenantId} ${DOCKER_IMAGE_NAME}
